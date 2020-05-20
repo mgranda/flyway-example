be5eb342e1a5833e6491c5d557a4dc8d30e7b14a
Jose Miguel Granda Castro <jgranda@ec.krugercorp.com>
2020-05-20 17:34:28 -0500

    Incluir soporte a base de datos Db2 en ambiente prod

--- environment.groovy
+++ environment.groovy
@@ -3,6 +3,7 @@ environments {
     common {
         flywayProperties {
             locations= "filesystem:src/main/resources/db/structure,filesystem:src/main/resources/db/data"
+            table = "SCHEMA_HISTORY"
         }
     }
 
@@ -12,7 +13,7 @@ environments {
             url="jdbc:h2:file:./target/devdb"
             user="sa"
             password=""
-            schemas="security"
+            schemas="DB_DEV_SCHEMA_VERSION"
         }
     }
 
@@ -22,17 +23,17 @@ environments {
             url="jdbc:h2:file:./target/qadb"
             user="user_qa"
             password=""
-            schemas="security"
+            schemas="DB_QA_SCHEMA_VERSION"
         }
     }
 
     pro {
         flywayProperties {
-            driver="org.h2.Driver"
-            url="jdbc:h2:file:./target/prodb"
-            user="user_pro"
+            driver="com.ibm.db2.jcc.DB2Driver"
+            url="jdbc:db2://localhost:50000/test"
+            user=""
             password=""
-            schemas="security"
+            schemas="DB_PRO_SCHEMA_VERSION"
         }
     }    
 }
---
+++ src/main/resources/db/data/V202005200001002__SCHEMANAME_INSERT_INTO_PERSON.sql
@@ -0,0 +1,3 @@
+insert into SMXMBASE.PERSON (ID, NAME) values (1, 'Axel');
+insert into SMXMBASE.PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into SMXMBASE.PERSON (ID, NAME) values (3, 'Ms. Bar');
